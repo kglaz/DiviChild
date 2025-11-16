33fbdd520decbf1132da87c2f263185c40459391
Krzysztof Głaz <132826044+kglaz@users.noreply.github.com>
2025-11-16 14:07:19 +0100

    Change logo container to h1 for accessibility
    Change the logo container from div to h1 for accessibility.

--- function_php-ustawienie-logotypu-jako-h1.php
+++ function_php-ustawienie-logotypu-jako-h1.php
@@ -1,1 +1,21 @@
+<?php
+// Aby zapewnić dostępność cyfrową musi pojawić się znacznik H! na górze strony.
+// Logotyp Divi nie ma tego znacznika, poniżej kod zamieniający jeden z kontenerów  
+// otaczających logotyp na H1.
 
+function change_logo_container_to_h1( $logo_html ) {
+
+    // Zamiana otwierającego kontenera
+    $logo_html = str_replace(
+        '<div class="logo_container">',
+        '<h1 class="logo_container">', 
+        $logo_html
+    );
+
+    // Zamiana zamykającego kontenera (tylko pierwsze wystąpienie)
+    // aby nie naruszyć innych divów Divi
+    $logo_html = preg_replace('/<\/div>/', '</h1>', $logo_html, 1);
+
+    return $logo_html;
+}
+add_filter( 'et_html_logo_container', 'change_logo_container_to_h1' );
